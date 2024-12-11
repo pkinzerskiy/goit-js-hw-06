ecebb6c23f36a3f00b124dda6830998f8dbccbde
P. Kinzerskiy <pkinzerskiy@gmail.com>
2024-12-11 16:13:03 +0200

    goit-js-hw-06

---
+++ js/task-2.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+
+class Storage {
+  #item;
+  constructor(items) {
+    this.#item = items;
+  };
+ 
+  getItems() {
+    return this.#item;
+  };
+
+  addItem(newItem) {
+    this.#item.push(newItem);
+  };
+
+  removeItem(itemToRemove) {
+    this.#item = this.#item.filter( el => el !== itemToRemove); 
+  };
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
